f229512850db13f86adc9266ec889acbb3f1ffb8
Ildar Meyker <ildar.meyker@gmail.com>
2021-11-02 16:09:21 +0300

    Add StickyTabs.

--- public/js/partyConfig.js
+++ public/js/partyConfig.js
@@ -130,14 +130,97 @@ PartyConfig.StickyTotal = {
     });
   }
 };
-PartyConfig.StickyHeader = {
-  init: function init() {}
+PartyConfig.StickyTabs = {
+  _$groups: $(),
+  _scrollHandlingFrozen: false,
+  _tabsOffset: 0,
+  _handleWindowScroll: function _handleWindowScroll(e) {
+    if (this._scrollHandlingFrozen) return;
+    var scrollTop = $(window).scrollTop();
+    var state = scrollTop > this._tabsOffset;
+    $("#party-config__tabs-fake").toggleClass("active", state);
+    $("#party-config__tabs").toggleClass("fixed", state);
+
+    this._setActiveGroup();
+  },
+  _handleWindowResize: function _handleWindowResize(e) {
+    this._updateTabsOffset();
+  },
+  _handleTabClick: function _handleTabClick(e) {
+    var _this = this;
+
+    e.preventDefault();
+    this._scrollHandlingFrozen = true;
+    $("html, body").animate({
+      scrollTop: $($(e.currentTarget).attr("href")).offset().top - 120
+    }, 500, function () {
+      _this._scrollHandlingFrozen = false;
+    });
+    var index = $(e.currentTarget).index();
+
+    this._setActiveTab(index);
+  },
+  _updateTabsOffset: function _updateTabsOffset() {
+    this._tabsOffset = $("#party-config__tabs-fake").offset().top;
+  },
+  _setActiveTab: function _setActiveTab(index) {
+    $("#party-config__tabs").children().removeClass("active history").eq(index).addClass("active").prevAll().addClass("history");
+  },
+  _setActiveGroup: function _setActiveGroup() {
+    var _this2 = this;
+
+    var percentages = [];
+
+    this._$groups.each(function (index, element) {
+      percentages.push(_this2._percentWithinViewport($(element)));
+    });
+
+    var indexOfMaxValue = percentages.reduce(function (iMax, x, i, arr) {
+      return x > arr[iMax] ? i : iMax;
+    }, 0);
+
+    this._setActiveTab(indexOfMaxValue);
+  },
+  _percentWithinViewport: function _percentWithinViewport($element) {
+    var elementTop = $element.offset().top;
+    var scrollTop = $(window).scrollTop();
+    var spaceTop = elementTop - scrollTop;
+    var elementHeight = $element.height();
+    var screenHeight = $(window).height();
+    var scrollBottom = scrollTop + screenHeight;
+    var bottomElement = elementTop + $element.height();
+    var spaceBottom = bottomElement - scrollBottom;
+    var heightInScreen = elementHeight - spaceBottom;
+    var percentage;
+
+    if (spaceTop < 0) {
+      heightInScreen -= spaceTop * -1;
+    }
+
+    if (spaceBottom < 0) {
+      heightInScreen -= spaceBottom * -1;
+    }
+
+    percentage = heightInScreen / screenHeight * 100;
+    percentage = percentage < 0 ? 0 : percentage;
+    return percentage;
+  },
+  init: function init() {
+    $(window).on("scroll", $.throttle(100, this._handleWindowScroll.bind(this)));
+    $(window).on("resize", $.throttle(250, this._handleWindowResize.bind(this)));
+    $(document).on("click", ".party-config__tabs__item", this._handleTabClick.bind(this));
+
+    this._updateTabsOffset();
+
+    this._$groups = $("#party-config__groups .party-config__group");
+  }
 };
 $(function () {
   PartyConfig.StickyTotal.init();
   PartyConfig.HeroesSlider.init();
-  PartyConfig.StickyHeader.init();
+  PartyConfig.StickyTabs.init();
 });
+window.PartyConfig = PartyConfig;
 
 /***/ })
 
--- src/js/partyConfig.js
+++ src/js/partyConfig.js
@@ -42,12 +42,130 @@ PartyConfig.StickyTotal = {
 	},
 };
 
-PartyConfig.StickyHeader = {
-	init() {},
+PartyConfig.StickyTabs = {
+	_$groups: $(),
+
+	_scrollHandlingFrozen: false,
+
+	_tabsOffset: 0,
+
+	_handleWindowScroll(e) {
+		if (this._scrollHandlingFrozen) return;
+
+		const scrollTop = $(window).scrollTop();
+		const state = scrollTop > this._tabsOffset;
+
+		$("#party-config__tabs-fake").toggleClass("active", state);
+		$("#party-config__tabs").toggleClass("fixed", state);
+
+		this._setActiveGroup();
+	},
+
+	_handleWindowResize(e) {
+		this._updateTabsOffset();
+	},
+
+	_handleTabClick(e) {
+		e.preventDefault();
+
+		this._scrollHandlingFrozen = true;
+		$("html, body").animate(
+			{
+				scrollTop:
+					$($(e.currentTarget).attr("href")).offset().top - 120,
+			},
+			500,
+			() => {
+				this._scrollHandlingFrozen = false;
+			}
+		);
+
+		const index = $(e.currentTarget).index();
+		this._setActiveTab(index);
+	},
+
+	_updateTabsOffset() {
+		this._tabsOffset = $("#party-config__tabs-fake").offset().top;
+	},
+
+	_setActiveTab(index) {
+		$("#party-config__tabs")
+			.children()
+			.removeClass("active history")
+			.eq(index)
+			.addClass("active")
+			.prevAll()
+			.addClass("history");
+	},
+
+	_setActiveGroup() {
+		const percentages = [];
+
+		this._$groups.each((index, element) => {
+			percentages.push(this._percentWithinViewport($(element)));
+		});
+
+		const indexOfMaxValue = percentages.reduce(
+			(iMax, x, i, arr) => (x > arr[iMax] ? i : iMax),
+			0
+		);
+
+		this._setActiveTab(indexOfMaxValue);
+	},
+
+	_percentWithinViewport($element) {
+		const elementTop = $element.offset().top;
+		const scrollTop = $(window).scrollTop();
+		const spaceTop = elementTop - scrollTop;
+		const elementHeight = $element.height();
+		const screenHeight = $(window).height();
+		const scrollBottom = scrollTop + screenHeight;
+		const bottomElement = elementTop + $element.height();
+		const spaceBottom = bottomElement - scrollBottom;
+		let heightInScreen = elementHeight - spaceBottom;
+		let percentage;
+
+		if (spaceTop < 0) {
+			heightInScreen -= spaceTop * -1;
+		}
+
+		if (spaceBottom < 0) {
+			heightInScreen -= spaceBottom * -1;
+		}
+
+		percentage = (heightInScreen / screenHeight) * 100;
+		percentage = percentage < 0 ? 0 : percentage;
+
+		return percentage;
+	},
+
+	init() {
+		$(window).on(
+			"scroll",
+			$.throttle(100, this._handleWindowScroll.bind(this))
+		);
+
+		$(window).on(
+			"resize",
+			$.throttle(250, this._handleWindowResize.bind(this))
+		);
+
+		$(document).on(
+			"click",
+			".party-config__tabs__item",
+			this._handleTabClick.bind(this)
+		);
+
+		this._updateTabsOffset();
+
+		this._$groups = $("#party-config__groups .party-config__group");
+	},
 };
 
 $(function () {
 	PartyConfig.StickyTotal.init();
 	PartyConfig.HeroesSlider.init();
-	PartyConfig.StickyHeader.init();
+	PartyConfig.StickyTabs.init();
 });
+
+window.PartyConfig = PartyConfig;
