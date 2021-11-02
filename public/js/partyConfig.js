65cd357bec3384969e439f6866a8d0920f3d8ab7
Ildar Meyker <ildar.meyker@gmail.com>
2021-11-02 09:31:12 +0300

    Add progress.

--- public/js/partyConfig.js
+++ public/js/partyConfig.js
@@ -93,10 +93,50 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
+var PartyConfig = {};
+PartyConfig.HeroesSlider = {
+  _$sliders: $(),
+  _destroySliders: function _destroySliders() {
+    try {
+      this._$sliders.slick("unslick");
+    } catch (e) {}
+  },
+  _initSliders: function _initSliders() {
+    this._$sliders.each(function () {
+      var $root = $(this).closest(".party-config__heroes");
+      $(this).slick({
+        prevArrow: $root.find(".party-config__heroes__prev"),
+        nextArrow: $root.find(".party-config__heroes__next"),
+        slidesToShow: 1,
+        dots: true
+      });
+    });
+  },
+  _onResize: function _onResize(mql) {
+    mql.matches ? this._initSliders() : this._destroySliders();
+  },
+  init: function init() {
+    this._$sliders = $(".party-config__heroes__row");
+    var mql = window.matchMedia("(max-width: 767.98px)");
+    mql.addListener(this._onResize.bind(this));
+
+    this._onResize(mql);
+  }
+};
+PartyConfig.StickyTotal = {
+  init: function init() {
+    new Sticksy(".js-sticky-widget", {
+      topSpacing: 20
+    });
+  }
+};
+PartyConfig.StickyHeader = {
+  init: function init() {}
+};
 $(function () {
-  new Sticksy(".js-sticky-widget", {
-    topSpacing: 20
-  });
+  PartyConfig.StickyTotal.init();
+  PartyConfig.HeroesSlider.init();
+  PartyConfig.StickyHeader.init();
 });
 
 /***/ })
--- src/js/partyConfig.js
+++ src/js/partyConfig.js
@@ -1,3 +1,53 @@
+const PartyConfig = {};
+
+PartyConfig.HeroesSlider = {
+	_$sliders: $(),
+
+	_destroySliders() {
+		try {
+			this._$sliders.slick("unslick");
+		} catch (e) {}
+	},
+
+	_initSliders() {
+		this._$sliders.each(function () {
+			const $root = $(this).closest(".party-config__heroes");
+			$(this).slick({
+				prevArrow: $root.find(".party-config__heroes__prev"),
+				nextArrow: $root.find(".party-config__heroes__next"),
+				slidesToShow: 1,
+				dots: true,
+			});
+		});
+	},
+
+	_onResize(mql) {
+		mql.matches ? this._initSliders() : this._destroySliders();
+	},
+
+	init() {
+		this._$sliders = $(".party-config__heroes__row");
+
+		const mql = window.matchMedia("(max-width: 767.98px)");
+
+		mql.addListener(this._onResize.bind(this));
+
+		this._onResize(mql);
+	},
+};
+
+PartyConfig.StickyTotal = {
+	init() {
+		new Sticksy(".js-sticky-widget", { topSpacing: 20 });
+	},
+};
+
+PartyConfig.StickyHeader = {
+	init() {},
+};
+
 $(function () {
-	new Sticksy(".js-sticky-widget", { topSpacing: 20 });
+	PartyConfig.StickyTotal.init();
+	PartyConfig.HeroesSlider.init();
+	PartyConfig.StickyHeader.init();
 });
